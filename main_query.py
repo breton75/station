256bf8ed68b96f0bc8aa420b3ecdca36095a98a6
breton75 <breton75@yandex.ru>
2018-04-16 23:26:56 +0300

    6

--- main_query.py
+++ main_query.py
@@ -7,34 +7,59 @@ WITH aaa as(
 	GROUP BY
 		RESERVID
 ),
-pers as (SELECT
-			aaa.pid as PERSID,
-			aaa.rid as RESERVID,
-			(
-				Admin.PERSON.SURNAME + ' ' + Admin.PERSON.FIRSTNAME
-			) as PERSNAME,
-			Admin.PERSON.PERSONNUM AS PERSONNUM,
-			Admin.PERSON.BIRTHDATE AS BIRTHDATE,
-			Admin.PERSON.COUNTRY AS COUNTRY,
-			Admin.PERSON.SEX AS SEX
-		FROM
-			aaa
-		LEFT JOIN Admin.PERSON on
-			aaa.pid = Admin.PERSON.ID)
-SELECT
---	count()
-	Admin.RESERVE.ID as RESERVID,
-	Admin.RESERVE.CREATEDTIME AS CREATEDTIME,
-	pers.PERSID,
-	pers.PERSNAME,
-	pers.BIRTHDATE,
-	pers.COUNTRY,
-	pers.SEX
+Segment AS(
+	SELECT
+		Admin.PERSONSEGMENTRELATION.SEGMENTID AS Id,
+		Admin.PERSONSEGMENT.NAME as Name,
+		Admin.PERSONSEGMENT.SHORTNAME as ShortName,
+		Admin.PERSONSEGMENTRELATION.PERSONID as PersonId
+	FROM
+		Admin.PERSONSEGMENTRELATION
+	LEFT JOIN Admin.PERSONSEGMENT on
+		Admin.PERSONSEGMENT.ID = Admin.PERSONSEGMENTRELATION.SEGMENTID
+),
+Person AS(
+	SELECT
+		aaa.pid as Id,
+		aaa.rid as ReservId,
+		(
+			Admin.PERSON.SURNAME + ' ' + Admin.PERSON.FIRSTNAME
+		) as Name,
+		Admin.PERSON.PERSONNUM AS Num,
+		Admin.PERSON.BIRTHDATE AS Birthdate,
+		Admin.PERSON.COUNTRY AS Country,
+		Admin.PERSON.SEX AS Sex,
+		Segment.Id AS SegmentId,
+		Segment.Name AS SegmentName,
+		Segment.ShortName AS SegmentShortName
+	FROM
+		aaa
+	LEFT JOIN Admin.PERSON on
+		aaa.pid = Admin.PERSON.ID
+	LEFT JOIN Segment ON
+		aaa.pid = Segment.PersonId
+) SELECT
+	--	count()
+	Admin.RESERVE.ID as ReservId,
+	Admin.RESERVE.CREATEDTIME AS CreatedDate,
+	Person.Id,
+	Person.Name,
+	Person.Birthdate,
+	Person.Country,
+	Person.Sex,
+	Person.SegmentId,
+	Person.SegmentName,
+	Person.SegmentShortName,
+	Admin.PERIOD.BEGINDATE,
+	Admin.PERIOD.ENDDATE
+--	Admin.PERIOD
 FROM
 	Admin.RESERVE
-LEFT JOIN pers ON Admin.RESERVE.ID = pers.RESERVID
+LEFT JOIN Person ON
+	Admin.RESERVE.ID = Person.ReservId
+LEFT JOIN Admin.PERIOD ON
+	Admin.PERIOD.RESERVID = Admin.RESERVE.ID
 WHERE
---	Admin.RESERVE.CREATEDTIME > '2018-04-01 00:00:00'
---	and 
-	Admin.RESERVE.RESNUM = 145780 
---Admin.RESERVE.ID
+	--	Admin.RESERVE.CREATEDTIME > '2018-04-01 00:00:00'
+ --	and 
+ Admin.RESERVE.RESNUM = 145780 --Admin.RESERVE.ID
